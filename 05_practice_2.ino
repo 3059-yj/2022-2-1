48701bdb87c8f836a6db90e73d2454a5d1cf60df
3059-yj <105334971+3059-yj@users.noreply.github.com>
2022-09-21 12:53:32 +0900

    GPIO 를 통한 LED 제어

---
+++ 05_practice_2.ino
@@ -0,0 +1,25 @@
+//05P12
+void setup() {
+  pinMode(7,OUTPUT);
+}
+
+void loop() {
+
+  digitalWrite(7, 1);
+  delay(1000);
+  digitalWrite(7, 0);
+  delay(1000);
+  int i;
+  for (i=0; i<=4; i++){
+  digitalWrite(7, 1);
+  delay(100);
+  digitalWrite(7, 0);
+  delay(100);
+    
+  }
+  while(1){
+    digitalWrite
+    (7, 1);} {
+    }
+
+}
